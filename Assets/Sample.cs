47e23e768e1a5176b989d35d5af3e5ee35ee842a
as <>
2021-01-21 05:26:06 +0900

    変数について

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Sample : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        string s = "あつ";
+        string myName = "atsu";
+        Debug.Log(s);
+        Debug.Log(myName);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0.1f, 0, 0);
+        
     }
 }
