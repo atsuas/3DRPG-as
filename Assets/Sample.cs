4bcb7ed2b88dd98083a13b2dd18b97e91fe2aac9
as <>
2021-01-21 17:08:48 +0900

    不要なスクリプトの削除

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -5,19 +5,6 @@ public class Sample : MonoBehaviour
     
     void Start()
     {
-        int n = 3;
-
-        if (n > 10)
-        {
-            Debug.Log("nは10より大きい");
-        }
-        else if (n > 0)
-        {
-            Debug.Log("nは0より大きい");
-        }
-        else
-        {
-            Debug.Log("その他");
-        }
+        
     }
 }
