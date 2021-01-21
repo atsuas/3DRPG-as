064c9ca74f8220077ba63730b37590d6329ba9ee
as <>
2021-01-22 05:06:25 +0900

    演習問題②

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -2,9 +2,12 @@
 
 public class Question : MonoBehaviour
 {
-    int[] numbers = { 10, 20, 30 };
-
+    int[] numbers = { 10, 20, 30, 1, 2, 33, -33, 44, 556, 54345, 3434 };
     void Start()
     {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Debug.Log(numbers[i]);
+        }
     }
 }
