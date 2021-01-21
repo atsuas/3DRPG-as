ca8cdcf24a48d3bfb3ae2b9d7b48740a5a209b82
as <>
2021-01-21 17:15:51 +0900

    switch文について

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -5,6 +5,21 @@ public class Sample : MonoBehaviour
     
     void Start()
     {
-        
+        string direction = "右";
+        switch (direction)
+        {
+            case "右":
+                Debug.Log("入力が右");
+                break;
+            case "左":
+                Debug.Log("入力が左");
+                break;
+            case "停止":
+                Debug.Log("入力が停止");
+                break;
+            default:
+                Debug.Log("その他");
+                break;  
+        }
     }
 }
