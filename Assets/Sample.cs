ee10662dbae5b156afd39e06bc78bc7f16fcbf8f
as <>
2021-01-21 16:57:17 +0900

    elseについて

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -5,12 +5,16 @@ public class Sample : MonoBehaviour
     // 123かける456割る2の計算結果をコンソールに表示したい
     void Start()
     {
-        int hp = -2;
+        int hp = 100;
 
         if (hp <= 0)
         {
             hp = 0;
+            Debug.Log("戦闘不能");
         }
-        Debug.Log(hp);
+        else
+        {
+            Debug.Log("生きている！");
+        }
     }
 }
