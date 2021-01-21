a5c3c754ab4ccb30f8ebe4af1c2196d07e668291
as <>
2021-01-21 21:43:36 +0900

    不要なスクリプトの削除

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -5,15 +5,6 @@ public class Sample : MonoBehaviour
     
     void Start()
     {
-        for (int i = 0; i < 5; i++)
-        {
-            if (i == 3)
-            {
-                //break; //処理を抜ける
-                continue; //一回飛ばす
-            }
-            
-        }
-        Debug.Log("魔法攻撃");
+        
     }
 }
