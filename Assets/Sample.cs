0803849277d7c7cf5c04bfa3b1785277dcfac04b
as <>
2021-01-22 05:16:12 +0900

    演習問題⑤

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 
 public class Question : MonoBehaviour
 {
-    List<string> nameList = new List<string>(){ "あつ", "atsu", "as" };
+    List<string> nameList = new List<string> { "スタジオしまづ", "嶋津恒彦", "しまづ" };
     void Start()
     {
+        // コンソールに0番目を表示したい
+        Debug.Log(nameList[0]);
+        // 0番目の値を"スタジオしまづアカデミア"に変更したい
+        nameList[0] = "スタジオしまづアカデミア";
+        // コンソールに0番目を表示したい
+        Debug.Log(nameList[0]);
     }
 }
