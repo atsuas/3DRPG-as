4d1dc1ee1ee8e5efe9bbf1e33f66ccd2f9872e07
as <>
2021-01-22 05:12:45 +0900

    演習問題④

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -1,16 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Question : MonoBehaviour
 {
-
-    int sum = 0;
-    int[] numbers = { 10, 20, 30, 1, 2, 33, -33, 44, 556, 54345, 3434, -1, 222, 333 };
+    List<string> nameList = new List<string>(){ "あつ", "atsu", "as" };
     void Start()
     {
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            sum += numbers[i];
-        }
-        Debug.Log(sum);
     }
 }
