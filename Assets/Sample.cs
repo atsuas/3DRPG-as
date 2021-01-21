25f4d567f61a4abf64581de387d11c98c08d728b
as <>
2021-01-21 22:06:48 +0900

    列挙型について

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -2,9 +2,20 @@
 
 public class Sample : MonoBehaviour
 {
-    
+    enum DIRECTION
+    {
+        STOP,
+        RIGHT,
+        LEFT,
+        TOP,
+        BOTTOM
+    }
+
+    DIRECTION direction;
+
     void Start()
     {
-        
+        direction = DIRECTION.LEFT;
+        Debug.Log(direction);
     }
 }
