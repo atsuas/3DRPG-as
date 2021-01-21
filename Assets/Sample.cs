a71a179ebf51be41b0819fc9b9bb7e35d1db7f33
as <>
2021-01-21 22:17:50 +0900

    列挙型について③

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -17,5 +17,24 @@ public class Sample : MonoBehaviour
     {
         direction = DIRECTION.LEFT;
         Debug.Log((int)direction);  //intを付けると配列の数値が返ってくる
+
+        switch (direction)
+        {
+            case DIRECTION.STOP:
+                Debug.Log("停止");
+                break;
+            case DIRECTION.RIGHT:
+                Debug.Log("右");
+                break;
+            case DIRECTION.LEFT:
+                Debug.Log("左");
+                break;
+            case DIRECTION.TOP:
+                Debug.Log("上");
+                break;
+            case DIRECTION.BOTTOM:
+                Debug.Log("下");
+                break;
+        }
     }
 }
