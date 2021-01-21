7ff962f4f222e1b482e2980876828ca7823a77ac
as <>
2021-01-22 05:23:56 +0900

    演習問題⑦

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -3,17 +3,12 @@ using UnityEngine;
 
 public class Question : MonoBehaviour
 {
-    List<string> nameList = new List<string> { "スタジオしまづ", "嶋津恒彦", "しまづ" };
+    List<string> nameList = new List<string> { "スタジオしまづ", "アンチスタジオしまづ", "しまづ" };
 
     void Start()
     {
-        // nameListに"スタジオしまづアカデミア"を追加したい：Addってのを使う
-        nameList.Add("スタジオしまづアカデミア");
-
-        // 追加したあと、全ての値をログに表示して追加されたのを確認したい
-        for (int i = 0; i < nameList.Count; i++)
-        {
-            Debug.Log(nameList[i]);
-        }
+        // nameListの"アンチスタジオしまづ"を消したい
+        nameList.Remove("アンチスタジオしまづ");
+        
     }
 }
