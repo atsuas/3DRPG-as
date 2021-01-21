6ca7cd06731db5df67e9f7ca1ee3f1afbbff5f78
as <>
2021-01-21 21:25:32 +0900

    while文について

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -5,6 +5,12 @@ public class Sample : MonoBehaviour
     
     void Start()
     {
-        
+        int mp = 10;
+        while (mp > 0)
+        {
+            mp--;   //mp消費  必ず書かないと無限ループになってUnityが止まってしまう
+            Debug.Log("mp消費");
+            Debug.Log("魔法攻撃");
+        }
     }
 }
