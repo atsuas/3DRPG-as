1bc9e51ee256db0c6e64716fec0f9a8e57a5ffd8
as <>
2021-01-21 22:22:59 +0900

    演習問題①

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -2,39 +2,12 @@
 
 public class Sample : MonoBehaviour
 {
-    enum DIRECTION
-    {
-        STOP,
-        RIGHT,
-        LEFT,
-        TOP,
-        BOTTOM
-    }
-
-    DIRECTION direction;
-
+    int hp = 100;
     void Start()
     {
-        direction = DIRECTION.LEFT;
-        Debug.Log((int)direction);  //intを付けると配列の数値が返ってくる
-
-        switch (direction)
+        if (hp <= 0)
         {
-            case DIRECTION.STOP:
-                Debug.Log("停止");
-                break;
-            case DIRECTION.RIGHT:
-                Debug.Log("右");
-                break;
-            case DIRECTION.LEFT:
-                Debug.Log("左");
-                break;
-            case DIRECTION.TOP:
-                Debug.Log("上");
-                break;
-            case DIRECTION.BOTTOM:
-                Debug.Log("下");
-                break;
+            Debug.Log("戦闘不能");
         }
     }
 }
