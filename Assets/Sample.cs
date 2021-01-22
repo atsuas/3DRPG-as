38363be40860857c83fd2453080e37e8bb339edb
as <>
2021-01-22 18:44:36 +0900

    不要なスクリプトの削除

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -1,14 +1,11 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Question : MonoBehaviour
+public class Sample : MonoBehaviour
 {
-    List<string> nameList = new List<string> { "スタジオしまづ", "アンチスタジオしまづ", "しまづ" };
-
+    
     void Start()
     {
-        // nameListの"アンチスタジオしまづ"を消したい
-        nameList.Remove("アンチスタジオしまづ");
         
     }
 }
