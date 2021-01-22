eb6e8bd328169aa391354e3a3fc31a37bceeebed
as <>
2021-01-22 21:03:07 +0900

    クラスについて

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -1,22 +1,27 @@
 ﻿using UnityEngine;
 
+//SampleクラスでEnemyの関数を実行する
 public class Sample : MonoBehaviour
 {
-    int[] numbers = { 1, 1, 2, 2 };
+    public Enemy enemy;  //Enemyの窓口を作る
+
+    int hp;
+
+    void Attack()
+    {
+        Debug.Log("攻撃");
+    }
+    void Heal()
+    {
+        Debug.Log("回復");
+    }
 
     void Start()
     {
-        Debug.Log(Mean(numbers));
-    }
-
-    float Mean(int[] numbers)
-    {
-        float sum = 0;
-        int count = numbers.Length;
-        for (int i = 0; i < count; i++)
-        {
-            sum += numbers[i];
-        }
-        return sum / count; // 合計/個数
+        //Attack();
+        //Heal();
+        //Debug.Log(hp);
+        enemy.Attack();
+        Debug.Log(enemy.hp);
     }
 }
