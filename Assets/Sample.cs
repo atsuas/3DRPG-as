fdc38a1916537a29abcf575a0dda9dee7044aaa1
as <>
2021-01-22 21:05:44 +0900

    演習問題①

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -1,27 +1,12 @@
 ﻿using UnityEngine;
 
-//SampleクラスでEnemyの関数を実行する
 public class Sample : MonoBehaviour
 {
-    public Enemy enemy;  //Enemyの窓口を作る
-
-    int hp;
-
-    void Attack()
-    {
-        Debug.Log("攻撃");
-    }
-    void Heal()
-    {
-        Debug.Log("回復");
-    }
+    public Player player;
 
     void Start()
     {
-        //Attack();
-        //Heal();
-        //Debug.Log(hp);
-        enemy.Attack();
-        Debug.Log(enemy.hp);
+        //ここでPlayerのAttack関数を実行したい
+        player.Attack();
     }
 }
