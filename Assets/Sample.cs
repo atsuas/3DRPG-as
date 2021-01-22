5a02b346c56e1c6a1478bcc9aefcdfe874d6c444
as <>
2021-01-22 19:19:11 +0900

    演習問題①

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -1,37 +1,19 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Sample : MonoBehaviour
+public class Question : MonoBehaviour
 {
-    
+
     void Start()
     {
-        //関数の使い方
-        //関数名();
-        //SayName();
-        //SayName1("あつ", 20);
-        string studioName = GetSayName1("atsu", 3);
-        Debug.Log(studioName);
-        
-    }
-    //何も返さないならvoid / string型を返すので　string 関数名()
-    string GetSayName1(string userName, int age)
-    {
-        //Debug.Log("スタジオ" + userName + age + "歳");
-        //Debug.Log(userName + "スタジオ");
-        return "スタジオ" + userName + age + "周年";
     }
 
-    //引数ありの関数の作成
-    void SayName1(string userName, int age)
+    void FirstFunc1()
     {
-        Debug.Log("スタジオ" + userName + age + "歳");
-        Debug.Log(userName + "スタジオ");
+
     }
 
-    //関数の作成 　void 関数名() {}
-    void SayName()
+    void FirstFunc2()
     {
-        Debug.Log("スタジオ");
-    }
+
+}
 }
