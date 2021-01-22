1499f4b811234647f4f3d6e8187734e51f6e6050
as <>
2021-01-22 21:52:16 +0900

    キーボード入力について

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -5,25 +5,25 @@ public class Sample : MonoBehaviour
     
     void Update()
     {
-        ////キーボードの横方向の入力を受け付ける
-        //float x = Input.GetAxis("Horizontal");
-        //Debug.Log(x);
-        ////上下の入力
-        //float z = Input.GetAxis("Vertical");
-        //Debug.Log(z);
+        //キーボードの横方向の入力を受け付ける
+        float x = Input.GetAxis("Horizontal");
+        Debug.Log(x);
+        //上下の入力
+        float z = Input.GetAxis("Vertical");
+        Debug.Log(z);
 
-        ////Rawは−１、０、１しか取らない
-        //float rawX = Input.GetAxisRaw("Horizontal");
-        //Debug.Log(rawX);
-        //float rawZ = Input.GetAxisRaw("Vertical");
-        //Debug.Log(rawZ);
+        //Rawは−１、０、１しか取らない
+        float rawX = Input.GetAxisRaw("Horizontal");
+        Debug.Log(rawX);
+        float rawZ = Input.GetAxisRaw("Vertical");
+        Debug.Log(rawZ);
 
         //ボタン入力
         if (Input.GetButtonDown("Jump"))
         {
             Debug.Log("Spaceを押した");
         }
-
+        // Jを押したとき
         if (Input.GetKeyDown(KeyCode.J))
         {
             Debug.Log("Jを押した");
