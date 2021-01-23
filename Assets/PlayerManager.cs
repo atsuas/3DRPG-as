82abd049971eb51ad5282a8a9e267fe36ed9aa37
as <>
2021-01-23 15:35:23 +0900

    ミニゲーム

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(x, 0, z) * 0.1f;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ゲーム終了");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
