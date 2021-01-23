51e660e6c503c8edd5a531cc5720cae6ab4ada4e
as <>
2021-01-23 18:29:28 +0900

    動作の切り替えについて  スクリプト

---
+++ Assets/Scripts/ArthurManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArthurManager : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            animator.SetFloat("MoveSpeed", 1.1f);
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            animator.SetFloat("MoveSpeed", 0);
+        }
+    }
+}
