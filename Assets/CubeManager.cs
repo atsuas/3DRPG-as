fec8e9eb1cd38bfb0d4b057fd976ca4132dbf4bc
as <>
2021-01-23 15:02:53 +0900

    triggerについて

--- Assets/CubeManager.cs
+++ Assets/CubeManager.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class CubeManager : MonoBehaviour
 {
-   
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ぶつかったよ");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ぶつかったよーー");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("ぶつかった");
+    }
 }
