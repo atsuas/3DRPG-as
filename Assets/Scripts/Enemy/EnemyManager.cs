54144ddc6a2db18988aa40718a010bd9fe2b63ac
as <>
2021-01-24 21:35:57 +0900

    nav Meshの設定

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
