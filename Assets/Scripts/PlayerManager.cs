14d8df2bdf7a6d4a8668e912324803d1805ed5a0
as <>
2021-01-24 16:53:22 +0900

    キャラクターの移動　スクリプト

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    float x;
+    float z;
+    public float moveSpeed = 2;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キーボード入力で移動させる
+        x =  Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //　速度設定
+        rb.velocity = new Vector3(x, 0, z) * moveSpeed;
+    }
+}
