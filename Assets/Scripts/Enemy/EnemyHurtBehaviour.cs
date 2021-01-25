c2eb9b029039021a06d2491b6e7e35774a74ab40
as <>
2021-01-26 03:57:11 +0900

    被ダメージアニメーションが後から起きるバグの修正

--- Assets/Scripts/Enemy/EnemyHurtBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyHurtBehaviour.cs
@@ -8,6 +8,7 @@ public class EnemyHurtBehaviour : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.ResetTrigger("Hurt");
         animator.GetComponent<NavMeshAgent>().speed = 0;
     }
 
@@ -18,10 +19,10 @@ public class EnemyHurtBehaviour : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Hurt");
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Assets/Scripts/Player/PlayerHurtBehaviour.cs
+++ Assets/Scripts/Player/PlayerHurtBehaviour.cs
@@ -7,6 +7,7 @@ public class PlayerHurtBehaviour : StateMachineBehaviour
     // アニメーション開始時に実行される
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.ResetTrigger("Hurt");
         animator.GetComponent<PlayerManager>().moveSpeed = 0.4f;
     }
 
@@ -19,6 +20,7 @@ public class PlayerHurtBehaviour : StateMachineBehaviour
     //　アニメーションの遷移が行われるとき
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.ResetTrigger("Hurt");
         animator.GetComponent<PlayerManager>().moveSpeed = 3;
     }
 
